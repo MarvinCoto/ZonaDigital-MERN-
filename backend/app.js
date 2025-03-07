2b5a1d4a61e588411cb2b478483bb720b3749c5a
Marvin Coto <20230644@ricaldone.edu.sv>
2025-03-07 11:24:30 -0600

    Avance 7/3/2025

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 // Importo todo lo de la librería de Express
 import express from "express";
+import productsRoutes from "./src/routes/products.js"
 
 //Creo una constante que es igual a la librería que importé
 const app = express();
 
+//Que acepte datos en json
+app.use(express.json());
+
 //Definimos las rutas de las funciones que tendrá la página web
 
-app.use("api/products")
+app.use("/api/products", productsRoutes)
 
 //Exporto la constante para poder usar express en otros archivos
 export default app;
