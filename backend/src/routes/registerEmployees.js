89b3e45cbd55da91eadead90cc67264ac47424ce
Marvin Coto <20230644@ricaldone.edu.sv>
2025-03-27 15:28:46 -0600

    Advance 27/3/2025

--- backend/src/routes/registerEmployees.js
+++ backend/src/routes/registerEmployees.js
@@ -3,6 +3,6 @@ import registerEmployeesController from "../controllers/registerEmployeesControl
 
 const router = express.Router();
 
-router.router("/").post(registerEmployeesController.register)
+router.route("/").post(registerEmployeesController.register)
 
 export default router
